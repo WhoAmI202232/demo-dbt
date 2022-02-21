0b937bf2a2fc4b082cb4f7d4e3c6a74d21ed1801
WhoAmI202232 <99409168+WhoAmI202232@users.noreply.github.com>
2022-02-22 00:00:28 +0200

    Create gdrive_downloader.py

---
+++ gdrive_downloader.py
@@ -0,0 +1,7 @@
+from zdrive import Downloader
+
+d = Downloader()
+
+# folder which want to download from Drive
+folder_id = '1rGM3x4wlxAm0HoJ-P6Oe2_R-ZQ6siruL'
+d.downloadFolder(folder_id, destinationFolder=./)
