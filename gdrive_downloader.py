c0e423ed596339829cf5d5aa185d56b2b55cb9e5
WhoAmI202232 <99409168+WhoAmI202232@users.noreply.github.com>
2022-02-22 00:03:34 +0200

    Update gdrive_downloader.py

--- gdrive_downloader.py
+++ gdrive_downloader.py
@@ -4,4 +4,4 @@ d = Downloader()
 
 # folder which want to download from Drive
 folder_id = '1rGM3x4wlxAm0HoJ-P6Oe2_R-ZQ6siruL'
-d.downloadFolder(folder_id, destinationFolder=./)
+d.downloadFolder(folder_id, destinationFolder='./'
